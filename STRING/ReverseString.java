0a5315a219ca7d9b07f1f595cbe308ea740dfc41
vinitapawar2007 <vinitapawar200721@gmail.com>
2026-06-22 16:03:03 +0530

    Add files via upload

---
+++ STRING/ReverseString.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class ReverseString {
+    
+    public static void main(String[] args) {
+
+        String str = "Vinita";
+        
+
+        for(int i = str.length() - 1; i >= 0; i--) {
+            System.out.print(str.charAt(i));
+        }
+    }
+}
+    
---
+++ STRING/StringLength.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class StringLength {
+    public static void main(String[] args) {
+
+        String str = "Java Programming";
+
+        System.out.println("Length = " + str.length());
+    }
+}
